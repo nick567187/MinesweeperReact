c3ad3c5237dcacbbb01c3ba82a55905132b9e020
nick567187 <nchang.edu@gmail.com>
2018-12-26 14:40:27 -0800

    finished minesweeper game. will now attempt to refactor app with a board component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -11,8 +11,10 @@ class App extends React.Component {
 			board: [],
 			cols: 0
 		}
+		this.neutralIcon = ' ';
+		this.mineIcon = '\\*/'; 
 		this.selectDifficulty = this.selectDifficulty.bind(this);
-		this.mineIcon = '\\*/' 
+		this.revealSquare = this.revealSquare.bind(this);
 	}
   
   selectDifficulty(e) {
@@ -56,7 +58,7 @@ class App extends React.Component {
 
     for (var i = 0; i < board.length; i++) {
     	for (var j = 0; j < board[i].length; j++) {
-    		board[i][j] = {i, j, revealed: true, neighbors: 0, value: ' '}
+    		board[i][j] = {i, j, revealed: false, neighbors: 0, value: this.neutralIcon}
     	}
     }
 
@@ -87,6 +89,7 @@ class App extends React.Component {
     	let j = option[1];
     	board[i][j].value = this.mineIcon
     }
+    // count neighbors
     for (var i = 0; i < board.length; i++) {
     	for (var j = 0; j < board[i].length; j++) {
 		    this.countNeighbors(i,j,board);
@@ -116,6 +119,57 @@ class App extends React.Component {
     }
     coordinate.neighbors = total;
   }
+  
+
+  gameOver() {
+  	let { board } = this.state;
+    for (let i = 0; i < board.length; i++) {
+    	for (let j = 0; j < board[i].length; j++) {
+		    this.setState((prevState) => {
+          let newBoard = prevState.board.slice();
+          newBoard[i][j].revealed = true;
+          return {
+          	board: newBoard
+          }
+		    });
+    	}
+    }
+  }
+
+  revealSquare(i,j) {
+  	let { board } = this.state;
+
+    if (board[i][j].value === this.mineIcon) {
+    	this.gameOver();
+    }
+
+    this.setState((prevState) => {
+      let newBoard = prevState.board.slice();
+      newBoard[i][j].revealed = true;
+      return {
+      	board: newBoard
+      }
+    }, () => {
+    	if (board[i][j].neighbors === 0) {
+        this.floodFill(i,j, board);
+    	};
+    });
+  }
+
+  floodFill(boardI,boardJ, board) {
+  	for (var xoff = -1; xoff <= 1; xoff++) {
+    	for (var yoff = -1; yoff <= 1; yoff++) {
+    		let i = boardI + xoff;
+    		let j = boardJ + yoff;
+    		if (i > -1 && i < board.length && j > -1 && j < board[0].length) {
+	    		let neighbor = board[i][j];
+	    		if (neighbor.value !== this.mineIcon && !neighbor.revealed) {
+            this.revealSquare(i,j);
+	    		}
+    		}
+    	}
+    }
+  }
 
 	render() {
     let { diffSelect, board, cols } = this.state;
@@ -140,7 +194,7 @@ class App extends React.Component {
 			      	{board.map(rows => {
 			      		return rows.map(item => {
 	                return (
-	                  <Cell i={item.i} j={item.j} value={item.value} neighbors={item.neighbors} revealed={item.revealed}/>                  
+	                  <Cell revealSquare={this.revealSquare} i={item.i} j={item.j} value={item.value} neighbors={item.neighbors} revealed={item.revealed}/>                  
 	                );
 	              })
 	            })}
--- client/components/cell.jsx
+++ client/components/cell.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import styles from './cell.css';
 
 const Cell = (props) => {
+	    let cellStyle;
 	    let view = ' ';
 	    if (props.revealed) {
 	    	view = props.neighbors > 0 ? props.neighbors : props.value;
-	    } 
+	    	cellStyle = styles.pressed;
+	    } else {
+	    	cellStyle = styles.cell;
+	    }
 	  return (
-	    <span className={styles.cell}> { view } </span>
-	  )	
+	    <span className={cellStyle} onClick={() => props.revealSquare(props.i, props.j)}> { view } </span>
+	  );	
 };
 
 export default Cell;
